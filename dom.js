643a8066277a4c9b15746e8cd1abf93d0402cb3a
misterrodger <johndavidrodger@gmail.com>
2019-03-21 11:52:00 +0200

    fixed cursor pointer, removed console logs and most comments Relates # 41

--- dom.js
+++ dom.js
@@ -1,7 +1,3 @@
-// part 2 linking it all together
-// The function here is called an iife,
-// it keeps everything inside hidden from the rest of our application
-
 (function() {
   // This is the dom node where we will keep our todo
   var todoContainer = document.getElementById('todo-container');
@@ -9,7 +5,6 @@
   var addTodoForm = document.getElementById('add-todo');
 
   var state = [{
-
       description: 'first todo',
       done: true,
       id: -3
@@ -24,16 +19,12 @@
       description: 'third todo',
       done: false
     },
-  ]; // this is our initial todoList
-
+  ]; // This is our initial todoList
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
     let todoNode = document.createElement('li');
-    // you will need to use addEventListener
 
-    // add span holding description
     let todoSpan = document.createElement('span');
-    console.log(todo.description);
 
     todoSpan.textContent = todo.description;
 
@@ -41,7 +32,7 @@
 
     // this adds the delete button
     let deleteButtonNode = document.createElement('button');
-    deleteButtonNode.textContent = "Delete"; // Insert text
+    deleteButtonNode.textContent = "Delete";
     todoNode.appendChild(deleteButtonNode);
 
     deleteButtonNode.addEventListener('click', function(event) {
@@ -51,11 +42,8 @@
     deleteButtonNode.classList.add("btn");
     todoNode.appendChild(deleteButtonNode);
 
-    // add markTodo button
-
-    // this adds the delete button
     let doneButtonNode = document.createElement('button');
-    doneButtonNode.textContent = "Done"; // Insert text
+    doneButtonNode.textContent = "Done";
     todoNode.appendChild(doneButtonNode);
 
     doneButtonNode.addEventListener('click', function(event) {
@@ -63,25 +51,19 @@
       update(newState);
     });
 
-
-    // add classes for css
     doneButtonNode.classList.add("btn");
 
     todoNode.appendChild(doneButtonNode);
     return todoNode;
   };
 
-  // bind create todo form
   if (addTodoForm) {
     addTodoForm.addEventListener('submit', function(event) {
-      // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-      // what does event.preventDefault do?
-      // what is inside event.target?
+
       event.preventDefault();
 
       let descriptionEle = document.getElementById("description"); // event.target ....
       let description = descriptionEle.value;
-      // hint: todoFunctions.addTodo
 
       if (description) {
         let newTodo = {
@@ -89,24 +71,17 @@
           done: false
         }
         state = todoFunctions.addTodo(state, newTodo);
-
         descriptionEle.value = "";
-        // ?? change this!
-
         update(state);
-
       }
     });
   }
 
-  // you should not need to change this function
   var update = function(newState) {
     state = newState;
-    console.log(state);
     renderState(state);
   };
 
-  // you do not need to change this function
   var renderState = function(state) {
 
     var todoListNode = document.createElement('ul');
@@ -114,13 +89,12 @@
 
     state.forEach(function(todo) {
       if (todo.done)
-        completedListNode.insertBefore(createTodoNode(todo),completedListNode.childNodes[0]);
+        completedListNode.insertBefore(createTodoNode(todo), completedListNode.childNodes[0]);
       else {
         todoListNode.appendChild(createTodoNode(todo));
       }
     });
 
-    // you may want to add a class for css
     todoContainer.replaceChild(todoListNode, todoContainer.lastChild);
     completedContainer.replaceChild(completedListNode, completedContainer.lastChild);
   };
--- logic.js
+++ logic.js
@@ -1,22 +1,13 @@
-// Part 1. Fill in any missing parts of the todoFunction object!
-// you can access these on todo.todoFunctions
-// For part one we expect you to use tdd
-
 var todoFunctions = {
-  // todoFunctions.generateId() will give you a unique id
-  // You do not need to understand the implementation of this function.
+
   generateId: (function() {
     var idCounter = 0;
-
     function incrementCounter() {
       return (idCounter += 1);
     }
-
     return incrementCounter;
   })(),
 
-  //cloneArrayOfObjects will create a copy of the todos array
-  //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo) {
       return JSON.parse(JSON.stringify(todo));
@@ -24,21 +15,12 @@ var todoFunctions = {
   },
 
   addTodo: function(todos, newTodo) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // returns a new array, it should contain todos with the newTodo added to the end.
-    // add an id to the newTodo. You can use the generateId function to create an id.
-    // hint: array.concat
     let newArr = this.cloneArrayOfObjects(todos);
     newTodo["id"] = this.generateId();
     newArr.push(newTodo);
     return newArr;
   },
   deleteTodo: function(todos, idToDelete) {
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // return a new array, this should not contain any todo with an id of idToDelete
-    // hint: array.filter
-
-
     var newtodos = todoFunctions.cloneArrayOfObjects(todos);
     return newtodos.filter(function(obj) {
       if (obj.id !== idToDelete) {
@@ -47,6 +29,7 @@ var todoFunctions = {
       return false;
     })
   },
+
   markTodo: function(todos, idToMark) {
     let result = todoFunctions.cloneArrayOfObjects(todos)
     let sorted = result.map(function(x) {
@@ -56,11 +39,6 @@ var todoFunctions = {
       return x;
     })
     return sorted
-
-    // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
-    // in the new todo array, all elements will remain unchanged except the one with id: idToMark
-    // this element will have its done value toggled
-    // hint: array.map
   },
 
   sortTodos: function(todos, sortFunction) {
@@ -71,10 +49,6 @@ var todoFunctions = {
   },
 };
 
-// Why is this if statement necessary?
-// The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details:
-// http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
 }
